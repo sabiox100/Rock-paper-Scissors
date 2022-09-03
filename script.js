ca5e39705fd83e46d42a94aa48cd7d40b73884da
sabiox100 <nathanielmuthomi4@gmail.com>
2022-09-03 14:57:53 +0300

    create dom methods for UI

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var choices = ['rock', 'paper','scissors'];
+let roundCount = 0;
 let playerCount = 0;
 let computerCount = 0;
 let tieCount = 0;
@@ -6,19 +7,15 @@ let Winner;
 let champion;
 
 
+
 function getComputerChoice() {
     computerChoice = choices[Math.floor(Math.random() * choices.length)];
     return computerChoice;
 }
-function getPlayerChoice() {
-    playerChoice = prompt("rock, paper or scissors?");
-    return playerChoice;
-}
 
 
-function playRound() {
+function playRound(playerChoice) {
     var computerChoice = getComputerChoice();
-    var playerChoice = getPlayerChoice();
 
     if (computerChoice === playerChoice) {
         Winner = 'This round was tie';
@@ -26,37 +23,32 @@ function playRound() {
     } else if (computerChoice == 'rock' ) {
         if (playerChoice == 'paper') {
             Winner = 'You won this round!';
-            playerCount++
+            playerCount++;
         } else {
             Winner = 'You lost this round!';
-            computerCount++
+            computerCount++;
         }
     } else if (computerChoice == 'paper') {
         if (playerChoice == 'scissors') {
             Winner = 'You won this round!';
-            playerCount ++
+            playerCount ++;
         } else {
             Winner = 'You lost this round!';
-            computerCount++
+            computerCount++;
         }
     } else if (computerChoice == 'scissors') {
         if (playerChoice == 'rock') {
             Winner = 'You won this round!';
-            playerCount++
+            playerCount++;
         } else {
             Winner = 'You lost this round!';
-            computerCount++
+            computerCount++;
         }   
     }
     return Winner;
 
 }
-function game() {
-    for (let i = 0; i < 5; i++) {
-        playRound();
-        alert( Winner + " play another round")
-    }
-} 
+
 
 function getChampion() {
     if (computerCount > playerCount) {
@@ -70,11 +62,62 @@ function getChampion() {
     return champion;
 }
 
-game();
-alert(getChampion());
+function game(playerChoice){
+    if (roundCount > 5){
+        reset();
+    } else if (roundCount < 5) {
+        playRound(playerChoice);
+        displayScores();
+    }  else if (roundCount = 5){
+        playRound(playerChoice);
+        getChampion();
+        document.getElementById('champion').innerHTML = champion;
+        displayScores();
+        document.getElementById('playAgain').innerHTML = "Press any button to play again";
+    } 
+
+}
+
+function reset () {
+    document.getElementById('champion').innerHTML = "";
+    document.getElementById('playAgain').innerHTML = "";
+    playerCount = 0;
+    computerCount = 0;
+    tieCount = 0;
+    roundCount = 0;
+    displayScores();
+}
 
 
-console.log("The computer count is " + computerCount);
-console.log("The player count is " + playerCount);
-console.log("the tie count is " + tieCount);
-console.log("The champion is " + champion);
+const rockBtn = document.querySelector('#rockBtn');
+rockBtn.addEventListener('click', () => {
+    let playerChoice = 'rock';
+    roundCount++;
+    game(playerChoice);
+    //playRound(playerChoice);
+ 
+});
+
+const paperBtn = document.querySelector('#paperBtn');
+paperBtn.addEventListener('click', () => {
+    let playerChoice = 'paper';
+    roundCount++;
+    game(playerChoice);
+
+});
+
+const scissorsBtn = document.querySelector('#scissorsBtn');
+scissorsBtn.addEventListener('click', () => {
+    let playerChoice = 'scissors';
+    roundCount++;
+    game(playerChoice);
+ 
+});
+function displayScores() {
+
+    console.log("The round count is " + roundCount);
+    document.getElementById('winner').innerHTML = Winner;
+    document.getElementById('playerScore').innerHTML = "The player Score is: " + playerCount;
+    document.getElementById('computerScore').innerHTML = "The computer Score is: " + computerCount;
+    document.getElementById('tieScore').innerHTML = "The Tie Score is: " + tieCount;
+}
